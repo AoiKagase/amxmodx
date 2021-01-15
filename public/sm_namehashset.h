02e056729fda53e9b3653834feb62fd892cacb8b
Aoi Kagase <datumoujidai@gmail.com>
2021-01-16 02:12:42 +0900

    W.I.P

--- public/sm_namehashset.h
+++ public/sm_namehashset.h
@@ -29,6 +29,7 @@
  * Version: $Id$
  */
 
+
 #ifndef _include_sourcemod_namehashset_h_
 #define _include_sourcemod_namehashset_h_
 
@@ -38,154 +39,152 @@
  * @brief Stores a set of uniquely named objects.
  */
 
-#include <am-allocator-policies.h>
-#include <am-hashmap.h>
-#include <am-string.h>
+#include <amtl/am-allocator-policies.h>
+#include <amtl/am-hashmap.h>
+#include <amtl/am-string.h>
 #include "sm_stringhashmap.h"
 
-// namespace SourceMod
-// {
+//namespace SourceMod
+//{
 
-	// The HashPolicy type must have these methods:
-	// 		static bool matches(const char *key, const T &value);
-	// 		static uint32_t hash(const CharsAndLength &key);
-	//
-	// Depending on what lookup types are used, and how hashing should be done.
-	// Most of the time, key hashing will just call the key's hash() method.
-	//
-	// If these members are available on T, then the HashPolicy type can be left
-	// default. It is okay to use |T *|, the functions will still be looked up
-	// on |T|.
-	template <typename T, typename KeyPolicy = T>
-	class NameHashSet : public ke::SystemAllocatorPolicy
+// The HashPolicy type must have this method:
+// 		static bool matches(const char *key, const T &value);
+//
+// Depending on what lookup types are used.
+//
+// If these members are available on T, then the HashPolicy type can be left
+// default. It is okay to use |T *|, the functions will still be looked up
+// on |T|.
+template <typename T, typename KeyPolicy = T>
+class NameHashSet : public ke::SystemAllocatorPolicy
+{
+	typedef detail::CharsAndLength CharsAndLength;
+
+	// Default policy type: the two types are different. Use them directly.
+	template <typename KeyType, typename KeyPolicyType>
+	struct Policy
 	{
-		typedef detail::CharsAndLength CharsAndLength;
+		typedef KeyType Payload;
 
-		// Default policy type: the two types are different. Use them directly.
-		template <typename KeyType, typename KeyPolicyType>
-		struct Policy
+		static uint32_t hash(const CharsAndLength &key)
 		{
-			typedef KeyType Payload;
-
-			static uint32_t hash(const CharsAndLength &key)
-			{
-				return KeyPolicyType::hash(key);
-			}
-
-			static bool matches(const CharsAndLength &key, const KeyType &value)
-			{
-				return KeyPolicyType::matches(key.c_str(), value);
-			}
-		};
-
-		// Specialization: the types are equal, and T is a pointer. Strip the
-		// pointer off so we can access T:: for match functions.
-		template <typename KeyType>
-		struct Policy<KeyType *, KeyType *>
-		{
-			typedef KeyType *Payload;
-
-			static uint32_t hash(const CharsAndLength &key)
-			{
-				return KeyType::hash(key);
-			}
-
-			static bool matches(const CharsAndLength &key, const KeyType *value)
-			{
-				return KeyType::matches(key.c_str(), value);
-			}
-		};
-
-		typedef ke::HashTable<Policy<T, KeyPolicy>, ke::SystemAllocatorPolicy> Internal;
-
-	public:
-		NameHashSet()
-		{
-			if (!table_.init())
-				this->reportOutOfMemory();
+			return key.hash();
 		}
 
-		typedef typename Internal::Result Result;
-		typedef typename Internal::Insert Insert;
-		typedef typename Internal::iterator iterator;
-
-		Result find(const char *aKey)
+		static bool matches(const CharsAndLength &key, const KeyType &value)
 		{
-			return table_.find(aKey);
+			return KeyPolicyType::matches(key.chars(), value);
 		}
-
-		Insert findForAdd(const char *aKey)
-		{
-			return table_.findForAdd(aKey);
-		}
-
-		template <typename U>
-		bool add(Insert &i, U &&value)
-		{
-			return table_.add(i, std::forward<U>(value));
-		}
-
-		bool retrieve(const char *aKey, T *value)
-		{
-			CharsAndLength key(aKey);
-			Result r = table_.find(aKey);
-			if (!r.found())
-				return false;
-			*value = *r;
-			return true;
-		}
-
-		template <typename U>
-		bool insert(const char *aKey, U &&value)
-		{
-			CharsAndLength key(aKey);
-			Insert i = table_.findForAdd(key);
-			if (i.found())
-				return false;
-			return table_.add(i, std::forward<U>(value));
-		}
-
-		bool contains(const char *aKey)
-		{
-			CharsAndLength key(aKey);
-			Result r = table_.find(aKey);
-			return r.found();
-		}
-
-		bool remove(const char *aKey)
-		{
-			CharsAndLength key(aKey);
-			Result r = table_.find(key);
-			if (!r.found())
-				return false;
-			table_.remove(r);
-			return true;
-		}
-
-		void remove(Result &r)
-		{
-			table_.remove(r);
-		}
-
-		void clear()
-		{
-			table_.clear();
-		}
-
-		size_t mem_usage() const
-		{
-			return table_.estimateMemoryUse();
-		}
-
-		iterator iter()
-		{
-			return iterator(&table_);
-		}
-
-	private:
-		Internal table_;
 	};
 
-// } // namespace SourceMod
+	// Specialization: the types are equal, and T is a pointer. Strip the
+	// pointer off so we can access T:: for match functions.
+	template <typename KeyType>
+	struct Policy<KeyType *, KeyType *>
+	{
+		typedef KeyType *Payload;
+
+		static uint32_t hash(const detail::CharsAndLength &key)
+		{
+			return key.hash();
+		}
+
+		static bool matches(const CharsAndLength &key, const KeyType *value)
+		{
+			return KeyType::matches(key.chars(), value);
+		}
+	};
+
+	typedef ke::HashTable<Policy<T, KeyPolicy>, ke::SystemAllocatorPolicy> Internal;
+
+public:
+	NameHashSet()
+	{
+		if (!table_.init())
+			this->reportOutOfMemory();
+	}
+
+	typedef typename Internal::Result Result;
+	typedef typename Internal::Insert Insert;
+	typedef typename Internal::iterator iterator;
+
+	Result find(const char *aKey)
+	{
+		return table_.find(aKey);
+	}
+
+	Insert findForAdd(const char *aKey)
+	{
+		return table_.findForAdd(aKey);
+	}
+
+	template <typename U>
+	bool add(Insert &i, U &&value)
+	{
+		return table_.add(i, ke::Forward<U>(value));
+	}
+
+	bool retrieve(const char *aKey, T *value)
+	{
+		CharsAndLength key(aKey);
+		Result r = table_.find(aKey);
+		if (!r.found())
+			return false;
+		*value = *r;
+		return true;
+	}
+
+	template <typename U>
+	bool insert(const char *aKey, U &&value)
+	{
+		CharsAndLength key(aKey);
+		Insert i = table_.findForAdd(key);
+		if (i.found())
+			return false;
+		return table_.add(i, std::forward<U>(value));
+	}
+
+	bool contains(const char *aKey)
+	{
+		CharsAndLength key(aKey);
+		Result r = table_.find(aKey);
+		return r.found();
+	}
+
+	bool remove(const char *aKey)
+	{
+		CharsAndLength key(aKey);
+		Result r = table_.find(key);
+		if (!r.found())
+			return false;
+		table_.remove(r);
+		return true;
+	}
+
+	void remove(Result &r)
+	{
+		table_.remove(r);
+	}
+
+	void clear()
+	{
+		table_.clear();
+	}
+
+	size_t mem_usage() const
+	{
+		return table_.estimateMemoryUse();
+	}
+
+	iterator iter()
+	{
+		return iterator(&table_);
+	}
+
+private:
+	Internal table_;
+};
+
+//}
 
 #endif // _include_sourcemod_namehashset_h_
